c5c2d50cde2e972efb4f28a7dcca443f10fd4615
HyunHee_LEE <dimplehh@naver.com>
2024-03-27 14:38:11 +0900

    타이틀 영상 더미로 넣었음... 반복재생되도록

--- DefaultWindow/Logo.cpp
+++ DefaultWindow/Logo.cpp
@@ -19,7 +19,7 @@ void CLogo::Initialize()
 	m_hVideo = MCIWndCreate(g_hWnd,
 							nullptr,
 							WS_CHILD | WS_VISIBLE | MCIWNDF_NOPLAYBAR,
-							L"../Video/Wildlife.wmv");
+							L"../Video/Title.wmv");
 	MoveWindow(m_hVideo, 0, 0, WINCX, WINCY, FALSE);
 
 	MCIWndPlay(m_hVideo);
@@ -32,6 +32,8 @@ int CLogo::Update()
 
 void CLogo::Late_Update()
 {
+	if (MCIWndGetLength(m_hVideo) <= MCIWndGetPosition(m_hVideo))
+		MCIWndPlay(m_hVideo);
 	if (CKeyMgr::CreateSingleTonInst()->GetKeyState(KEY::ENTER) == KEY_STATE::TAP)
 	{
 		CSceneMgr::Get_Instance()->Scene_Change(SC_STAGE);
